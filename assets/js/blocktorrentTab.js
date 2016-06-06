f270e181f60bf0749ba38170444a32ccf0c67fd6
vthglyk <glykantv@student.ethz.ch>
2016-06-06 07:47:10 +0300

    Results, Project structure and more

---
+++ assets/js/blocktorrentTab.js
@@ -0,0 +1,20 @@
+function openblocktorrentTab(evt, tabName) {
+    // Declare all variables
+    var i, innertabcontent, tablinks;
+
+    // Get all elements with class="innertabcontent" and hide them
+    innertabcontent = document.getElementsByClassName("blocktorrentTabContent");
+    for (i = 0; i < innertabcontent.length; i++) {
+        innertabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("blocktorrentTab");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" special", "");
+    }
+
+    // Show the current tab, and add an "active" class to the link that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " special";
+}
---
+++ assets/js/minersTab.js
@@ -0,0 +1,20 @@
+function openMinersTab(evt, tabName) {
+    // Declare all variables
+    var i, innertabcontent, tablinks;
+
+    // Get all elements with class="innertabcontent" and hide them
+    innertabcontent = document.getElementsByClassName("minersTabContent");
+    for (i = 0; i < innertabcontent.length; i++) {
+        innertabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("minersTab");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" special", "");
+    }
+
+    // Show the current tab, and add an "active" class to the link that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " special";
+}
